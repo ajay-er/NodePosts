d32cf66536fba809c4db888da04de595f6f8ecc5
ajay <ajayzyan100@gmail.com>
2024-04-23 16:39:55 +0530

    feat: database connection

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,34 @@
 import { env } from '@/common/utils/envConfig';
 import { app, logger } from '@/server';
 
-const server = app.listen(env.PORT, () => {
-  const { NODE_ENV, HOST, PORT } = env;
-  logger.info(`Server (${NODE_ENV}) running on port http://${HOST}:${PORT}`);
-});
+import mongoConnect from './db/connection';
 
-const onCloseSignal = () => {
-  logger.info('sigint received, shutting down');
-  server.close(() => {
-    logger.info('server closed');
-    process.exit();
-  });
-  setTimeout(() => process.exit(1), 10000).unref(); // Force shutdown after 10s
+const startServer = async () => {
+  try {
+    const { NODE_ENV, HOST, PORT, MONGO_URI } = env;
+
+    await mongoConnect(MONGO_URI);
+
+    const server = app.listen(env.PORT, () => {
+      logger.info(`Server (${NODE_ENV}) running on port http://${HOST}:${PORT}`);
+    });
+
+    const onCloseSignal = () => {
+      logger.info('sigint received, shutting down');
+      server.close(() => {
+        logger.info('server closed');
+        process.exit();
+      });
+      setTimeout(() => process.exit(1), 10000).unref();
+    };
+
+    // Handle SIGINT and SIGTERM signals
+    process.on('SIGINT', onCloseSignal);
+    process.on('SIGTERM', onCloseSignal);
+  } catch (error) {
+    logger.error('Error connecting to MongoDB:', error);
+    process.exit(1); // Exit the process with an error status
+  }
 };
 
-process.on('SIGINT', onCloseSignal);
-process.on('SIGTERM', onCloseSignal);
+startServer();
